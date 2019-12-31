8e68cd736cb25f165750957f3253138076c7fcc2
kimikage <kimikage.ceo@gmail.com>
2019-12-31 17:53:02 +0900

    Add AxisArrays

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using Issue34121, FileIO, ColorTypes
+using AxisArrays
 using Test
 
 @testset "Issue34121" begin
     save("test.png", rand(Gray, 8, 8))
     img = load("test.png")
+    AxisArray(img, :G)
 end
