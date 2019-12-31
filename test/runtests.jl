d4f5c187898c7fb5c86669bd5c04a22ac2209cbf
kimikage <kimikage.ceo@gmail.com>
2019-12-31 17:16:50 +0900

    Remove TestImages

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
-using Issue34121, TestImages
+using Issue34121, FileIO, ColorTypes
 using Test
 
 @testset "Issue34121" begin
-    img = testimage("cameraman")
+    save("test.png", rand(Gray, 8, 8))
+    img = load("test.png")
 end
