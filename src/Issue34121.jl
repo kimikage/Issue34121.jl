fa42fa787cf7585e2da3d941bb2956f0abf57571
kimikage <kimikage.ceo@gmail.com>
2019-12-31 15:01:27 +0900

    Check the effect of method redefinition

--- src/Issue34121.jl
+++ src/Issue34121.jl
@@ -1,5 +1,9 @@
 module Issue34121
 
-using ImageFiltering
+if isdefined(Broadcast, :_sametype) && !isdefined(Broadcast, :axistype)
+    axistype(a::T, b::T) where T = a
+    axistype(a, b) = UnitRange{Int}(a)
+    Broadcast._bcs1(a, b) = Broadcast._bcsm(b, a) ? axistype(b, a) : (Broadcast._bcsm(a, b) ? axistype(a, b) : throw(DimensionMismatch("arrays could not be broadcast to a common size")))
+end
 
 end
